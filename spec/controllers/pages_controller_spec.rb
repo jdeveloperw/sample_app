3f7038c6d15aaf943e609bedcdb7ab74bbc9655d
JD White <jdeveloperw@gmail.com>
2011-07-18 18:20:27 -0700

    End of Chapter 3.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
+	pages = {"home" => "Home", "contact" => "Contact", "about" => "About"}
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-  end
+	pages.each_pair do |key,val|
+		describe "GET '#{key}'" do
+			it "should be successful" do
+				get "#{key}"
+				response.should be_success
+			end
 
+			it "should have the right title" do
+				get "#{key}"
+				response.should have_selector("title",
+					  :content => "Ruby on Rails Tutorial Sample App | #{val}")
+			end
+		end
+	end
 end
