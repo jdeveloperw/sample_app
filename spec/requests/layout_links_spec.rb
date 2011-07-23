ff441fc697ab939fba0d9958bf9ba6af1ee869aa
JD White <jdeveloperw@gmail.com>
2011-07-22 17:52:39 -0700

    End of Ch.5 + Exercises.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -5,7 +5,13 @@ describe "LayoutLinks" do
 
 	pages.each_pair do |key,val|
 		it "should have a #{val} page at '#{key}'" do
-			get "#{key}"
+			get key
+			response.should have_selector('title', :content => val)
+		end
+
+		it "should have the right links on the layout" do
+			visit root_path
+			click_link val
 			response.should have_selector('title', :content => val)
 		end
 	end
